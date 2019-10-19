39a784df74c53d40ab118ac6e443199ce7f73daa
Emmanuel Sales <hungbruno@gmail.com>
2019-10-18 21:57:10 -0300

    Projeto criado

---
+++ src/test/java/com/emmanuel/project/ProjetomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emmanuel.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
