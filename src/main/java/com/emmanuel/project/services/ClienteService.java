0f30199a446b45c4e3ea926b480a6ebcc1a29fef
Emmanuel Sales <hungbruno@gmail.com>
2019-10-30 22:00:33 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/emmanuel/project/services/ClienteService.java
@@ -0,0 +1,27 @@
+package com.emmanuel.project.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.emmanuel.project.domain.Cliente;
+import com.emmanuel.project.repositories.ClienteRepository;
+import com.emmanuel.project.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	public ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	
+	}
+	
+		
+	
+}
