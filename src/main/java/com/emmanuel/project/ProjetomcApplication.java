4d1579c8f75d2ac52a3820be85594591c9b401b2
Emmanuel Sales <hungbruno@gmail.com>
2019-11-21 00:03:39 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/emmanuel/project/ProjetomcApplication.java
+++ src/main/java/com/emmanuel/project/ProjetomcApplication.java
@@ -13,6 +13,7 @@ import com.emmanuel.project.domain.Cidade;
 import com.emmanuel.project.domain.Cliente;
 import com.emmanuel.project.domain.Endereco;
 import com.emmanuel.project.domain.Estado;
+import com.emmanuel.project.domain.ItemPedido;
 import com.emmanuel.project.domain.Pagamento;
 import com.emmanuel.project.domain.PagamentoComBoleto;
 import com.emmanuel.project.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.emmanuel.project.repositories.CidadeRepository;
 import com.emmanuel.project.repositories.ClienteRepository;
 import com.emmanuel.project.repositories.EnderecoRepository;
 import com.emmanuel.project.repositories.EstadoRepository;
+import com.emmanuel.project.repositories.ItemPedidoRepository;
 import com.emmanuel.project.repositories.PagamentoRepository;
 import com.emmanuel.project.repositories.PedidoRepository;
 import com.emmanuel.project.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class ProjetomcApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetomcApplication.class, args);
@@ -96,7 +100,7 @@ public class ProjetomcApplication implements CommandLineRunner {
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh/;mm");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
 		
 		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2019 10:32"), cli1, e1);
 		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2019 19:35"), cli1, e2);
@@ -111,6 +115,19 @@ public class ProjetomcApplication implements CommandLineRunner {
 		
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+		
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+		
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+		
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+		
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 
 	}
 
